f3ec592e6d57c8447b0e37d55657272fd282842a
hlsm <tamhlsmxq@gmail.com>
2026-07-17 13:41:32 +0700

    edit

--- auth-guard.js
+++ auth-guard.js
@@ -6,7 +6,7 @@ const ADMIN_EMAILS = ["admin@meter.com", "palamy@gmail.com"];
 
 onAuthStateChanged(auth, (user) => {
     // 2. ຖ້າຍັງບໍ່ລັອກອິນ ໃຫ້ກັບໄປໜ້າ Login (index.html)
-    if (!user) {["user1@MediaStream.com"];
+    if (!user) {
         window.location.href = 'index.html';
         return;
     }
