906696d8fd7d293556a7bd4e6fb0cf488a555d01
hlsm <tamhlsmxq@gmail.com>
2026-07-20 19:18:36 +0700

    ອ

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore, collection, addDoc, getDocs, query, where, orderBy, doc, deleteDoc, updateDoc, limit, getDoc, increment } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBADqyoayOct3GhXNn-a0r9Q7guwtfu2Gk",
+    apiKey: "AIzaSyBAdQyoayOct3GhXNn-a0r9Q7guwtfu2Gk",
     authDomain: "water-meter-a8b4a.firebaseapp.com",
     projectId: "water-meter-a8b4a",
-    storageBucket: "water-meter-a8b4a.appspot.com",
+    storageBucket: "water-meter-a8b4a.firebasestorage.app",
     messagingSenderId: "909213678508",
     appId: "1:909213678508:web:87e3b2aaf2f084aa8d5809"
 };
@@ -15,5 +15,4 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// ຢືນຢັນວ່າ Export ອອກໄປແບບນີ້ (ບໍ່ມີຄຳວ່າ default)
-export { auth, db };
+export { auth, db, signInWithEmailAndPassword, signOut, onAuthStateChanged, collection, addDoc, getDocs, query, where, orderBy, doc, deleteDoc, updateDoc, limit, getDoc, increment };
